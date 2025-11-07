d12a5d58814f03aab8dccfe22894ffb285364ccb
Leonit Fetahu <leonit.fetahu12@gmail.com>
2025-11-07 23:11:19 +0100

    needs testing

--- login.js
+++ login.js
@@ -1,25 +1,84 @@
 
-
 const SUBMIT = document.querySelector("#Submit");
 const LOGin = document.querySelector("#LOGIN");
 
 LOGin.classList.add("hidden");
 
-let user = false;
+// get data from localStorage
+const storedUser = JSON.parse(localStorage.getItem("SIGNIN"));
 
- SUBMIT.addEventListener("click",function(){
-    user = true;
-    window.location.href("final.html");
+SUBMIT.addEventListener("click", function (e) {
+  e.preventDefault();
 
-
- })
+  // we created these 2 variables bcs of inputs we had on login.html and this gets them and only shows theyr values in screen 
+ const InputF = document.querySelector("#Fname").value;
+ const InputL = document.querySelector("Lname").value;
  
- setInterval(()=>{
+ // first we use !storedUser to see if it works
 
-    if(user !== SUBMIT ){
+ if(!storedUser){
+alert("Please Log in ");
+return;
+ }
+ 
+ // then we compare if the object data that was stored in localstroage and then when we parsed from previus page we compare if its same 
+  if(InputF === storedUser.firstName && InputL === storedUser.lastName){
+alert("Please wait for verification");
+setTimeout(() => {window.location.href = "final.html"},400);
 
-    alert("Please press the Submit");
+ }
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const SUBMIT = document.querySelector("#Submit");
+// const LOGin = document.querySelector("#LOGIN");
+
+// LOGin.classList.add("hidden");
+
+// let user = false;
+
+//  SUBMIT.addEventListener("click",function(){
+//     user = true;
+//     window.location.href("final.html");
+
+
+//  })
+
+
+// const Local = JSON.parse(localStorage.getItem("SIGNIN"));
+
+
+
+ 
+//  setInterval(()=>{
+
+//     if(user !== SUBMIT ){
+
+//     alert("Please press the Submit");
     
-}
+// }
 
-}, 30000);
+// }, 30000);
