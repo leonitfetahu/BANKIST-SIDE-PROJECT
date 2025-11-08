40b5ff7d6813b627f7b24cb809493382a90bf579
Leonit Fetahu <leonit.fetahu12@gmail.com>
2025-11-08 17:49:26 +0100

    aded a goback button that sends u back at login

---
+++ final.js
@@ -0,0 +1,15 @@
+const btnExit = document.getElementById("EXIT");
+
+
+
+
+alert("verification completed");
+
+const GOback = function(){
+window.location.href = "login.html";
+
+}
+
+
+
+btnExit.addEventListener("click", GOback);
