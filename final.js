050e1b02e62f17a52a76905400a87ef6cb05df1e
Leonit Fetahu <leonit.fetahu12@gmail.com>
2025-12-11 22:51:53 +0100

    Admin UI almost finished

--- final.js
+++ final.js
@@ -1,19 +1,43 @@
 const btnExit = document.getElementById("EXIT");
 const balanceInput = document.querySelector("#Balance");
 const depositInput = document.querySelector("#Deposit"); 
-const btnSend = document.querySelector("#Send");
-
-alert("verification completed");
-
-
-
-
-
-
- btnSend.addEventListener("click", function(){
+const btnSend = document.querySelector("#Send"); //send money button
+const HiddenContainer = document.querySelector(".container2");
+const DropDownList = document.querySelector("#LocalAccounts"); //the drop down list
+// alert("verification completed");
+HiddenContainer.style.display = "none";
+DropDownList.classList.add("hidden");
 
 
- })
+
+
+// WHEN USER CLICKS ON THE BUTTON SEND THISIS THE MAIN EVENHANDLER
+btnSend.addEventListener("click",function(e){
+    e.preventDefault();
+  HiddenContainer.style.display = "flex";  // restores proper flex layout
+DropDownList.classList.remove("hidden");
+   
+const LocalStorageAccounts = JSON.parse(localStorage.getItem("SIGNIN")) || [];
+DropDownList.innerHTML ="";
+
+LocalStorageAccounts.forEach((acc)=>{
+if(acc.firstname&&acc.lastname){
+const Fname = document.createElement("option");
+Fname.textContent = acc.firstname + " " + acc.lastname;
+DropDownList.appendChild(Fname);
+    
+}
+
+
+
+})
+
+
+
+})
+
+
+
 
 
 const GOback = function(){
